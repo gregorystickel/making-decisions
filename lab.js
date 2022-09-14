fc0b75b221139c7a13cde4524af146cd821c8da3
Greg Stickel <gregory.stickel@live.com>
2022-09-14 15:19:56 -0400

    Question 9

--- lab.js
+++ lab.js
@@ -128,6 +128,14 @@ while (score < passingScore) {
 */
 
 //CODE HERE
+let changeMyMind = true;
+console.log(changeMyMind);
+if (changeMyMind === true) {
+  changeMyMind = false;
+} else if (changeMyMind === false) {
+  changeMyMind = true;
+}
+console.log(changeMyMind);
 
 ////////// PROBLEM 10 //////////
 // Using the "not" operator (!), change the current value of changeMyMind to true (it should currently be set to false due to the if-else statement in Problem 9). After you change the value of changeMyMind, console.log it's new value (it should now read true).
